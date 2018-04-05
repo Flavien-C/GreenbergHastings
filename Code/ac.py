4c0bb0de5f1a33e71db27a4af44a4a9a5016f46d
Flavien-C <flavcast.job@gmail.com>
2018-04-05 06:08:41 +0200

    Implementation of functionnality. TODO : Update method transition of CycleACGH in ac.py to count the number of neighbour is 1.

--- Code/ac.py
+++ Code/ac.py
@@ -4,7 +4,7 @@ class CA2D(object):
     def __init__(self,tail,nbEtats):
         self.nbEtats = nbEtats
         self.tail = tail
-        self.rayon = 1        
+        self.rayon = 1     
         self.initAlea()
 
     def __repr__(self):
@@ -16,7 +16,7 @@ class CA2D(object):
         return res 
 
     def initAlea(self):
-        self.conf = np.zeros((self.tail,self.tail),dtype=int)
+        self.conf = np.random.randint(0,self.nbEtats,size=(self.tail,self.tail))
 
     def calculPred(self):
         r = self.rayon
@@ -39,25 +39,65 @@ class GreenbergHastings(CA2D):
          self.seuil = 1
 
     def voisins(self,lig,col):
-        """
-        À DÉFINIR
-        """
-        return None
+        nord = self.pred[lig+1,col+1]
+        sud = self.pred[lig+2,col+1]
+        ouest = self.pred[lig+1,col]
+        est = self.pred[lig+1,col+2]
+        return np.array([nord,sud,ouest,est])
 
     def transition(self,lig,col):
         etatCour = self.conf[lig,col]
         voisins = self.voisins(lig,col)
-        """
-        À COMPLÉTER
-        """
-        
+        if etatCour == 1:
+            self.conf[lig,col] = 2
+        elif etatCour == 2:
+            self.conf[lig,col] = 0
+        else:
+            if 1 in voisins:
+                self.conf[lig,col] = 1
         
 
 class CycleACGH(CA2D):
     def __init__(self,tail,nbEtats,rayon,seuil):
-        pass
+        CA2D.__init__(self, tail,nbEtats)
+        self.seuil = seuil
+        self.rayon = rayon
+    
+    def voisins(self,lig,col):
+        return self.pred[lig:lig+2*self.rayon+1, col:col+2*self.rayon+1]
+    
+    def transition(self,lig,col):
+        etatCour = self.conf[lig,col]
+        voisins = self.voisins(lig,col)
+        if etatCour != 0:
+            self.conf[lig,col] = (self.conf[lig,col]+1) % self.nbEtats
+        else:
+            if 1 in voisins:
+                self.conf[lig,col] = 1
+            
+        
 
 
 
 if '__main__'==__name__:
-    pass
+    #grh = GreenbergHastings(4)
+    #print(grh)
+    #grh.calculPred()
+    #print(grh.pred)
+    
+    #print(grh.voisins(0,0))
+    #grh.transition(0,0)
+    #print(grh)
+    #for step in range(5):
+    #    grh.etape()
+    #    print(grh)    
+    
+    acgh = CycleACGH(7, 8, 3, 5)
+    print(acgh)
+    acgh.calculPred()
+    #print(acgh.voisins(0,0))
+    #acgh.transition(0,0)
+    #print(acgh)
+    #first trans
+    acgh.etape()
+    print(acgh)
